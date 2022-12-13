61d1b65c8ba3f27d2125c57b07c79ec692114073
sroy1999-dev <roysuvam1999@gmail.com>
2022-12-14 01:50:11 +0530

    new folders added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.scss';
 import Header from "./components/Header/Header";
+import Profile from './components/Profile/Profile';
+import Learnings from './components/Learnings/Learnings';
+import Testimonials from './components/Testimonials/Testimonials';
 import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Profile />
+      <Learnings />
+      <Testimonials />
       <Footer />
     </div>
   );
---
+++ src/components/Learnings/Learnings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Learnings.scss';
+
+const Learnings = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Learnings;
