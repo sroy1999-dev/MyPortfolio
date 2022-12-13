5edb21d66e4b5ee145a3d98d3c6decf320000a5e
sroy1999-dev <roysuvam1999@gmail.com>
2022-12-13 23:21:08 +0530

    new folders and files added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            Header
+        </header>
+    );
+};
+
+export default Header;
