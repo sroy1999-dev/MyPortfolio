4a3a7a071bc841e74505c19a8837da2907615551
sroy1999-dev <roysuvam1999@gmail.com>
2022-12-15 23:48:41 +0530

    new commits

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './Header.scss';
+import { AiOutlineAppstore } from 'react-icons/ai';
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+    const [display, setDisplay] = useState(true);
+    const handleClick = (e) => {
+        e.preventDefault();
+        setOpen(!open);
+        setDisplay(!display);
+    }
     return (
         <header>
-            Header
+            <h1>Suvam Roy</h1>
+            <ul className={open ? 'navlist other-devices' : 'navlist'}>
+                <li>Home</li>
+                <li>About</li>
+                <li>Learnings</li>
+                <li>Projects</li>
+                <li>Testimonials</li>
+                <li>Contacts</li>
+            </ul>
+            <AiOutlineAppstore className={display ? 'app' : 'app close'} onClick={handleClick} />
         </header>
     );
 };
